ad1dc3f200f432cd24792ca82bfcc6563a218d48
TechyDavid <drufus015@gmail.com>
2024-08-19 20:00:54 +0100

    NewPost

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import App from './App.jsx'
+import NewPost from './Components/NewPost.jsx';
 import './index.css'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App/> }, //App should be done here if the path is nothing//
+  { path: '/Create-post', element: <NewPost /> }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
